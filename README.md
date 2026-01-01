64d034597c44f58ba98cd7265785eea2b87813e5
Basil Crow <me@basilcrow.com>
2025-12-31 16:07:09 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# LXQt Universal Wayland Session
+
+A Wayland session for LXQt managed by [Universal Wayland Session
+Manager](https://github.com/Vladimir-csp/uwsm) (UWSM).
+
+### Prerequisites
+
+- [LXQt](https://lxqt-project.org/)
+- [UWSM](https://github.com/Vladimir-csp/uwsm)
+
+The following compositors are currently supported:
+
+- [labwc](https://github.com/labwc/labwc)
+
+This project aims to eventually support all the compositors and
+configurations currently provided in
+[lxqt-wayland-session](https://github.com/lxqt/lxqt-wayland-session).
+Pull requests are welcome.
+
+### Installation
+
+```sh
+meson setup --prefix=/usr/local build
+meson install -C build
+```
+
+### Usage
+
+Ensure you have logged into LXQt at least once. Then log into the new **LXQt
+(labwc, uwsm-managed)** session:
+
+```sh
+uwsm start -a -D LXQt -- labwc.desktop
+```
+
+The first time you log in, enable the following services:
+
+```sh
+systemctl --user enable --now lxqt-desktop.service
+systemctl --user enable --now lxqt-globalkeyshortcuts.service
+systemctl --user enable --now lxqt-notifications.service
+systemctl --user enable --now lxqt-panel.service
+systemctl --user enable --now lxqt-policykit-agent.service
+systemctl --user enable --now lxqt-powermanagement.service
+systemctl --user enable --now lxqt-runner.service
+systemctl --user enable --now lxqt-xscreensaver-autostart.service
+```
---
+++ meson-install-overrides.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -eu
+
+src="${MESON_SOURCE_ROOT}/systemd/user"
+dest="${MESON_INSTALL_DESTDIR_PREFIX}/lib/systemd/user"
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2ddesktop@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2ddesktop@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dglobalkeyshortcuts@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dglobalkeyshortcuts@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dnotifications@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dnotifications@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dpanel@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dpanel@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dpolicykit-agent@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dpolicykit-agent@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dpowermanagement@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dpowermanagement@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2drunner@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2drunner@autostart.service.d/override.conf'
+
+install -Dm644 \
+	"$src/"'app-lxqt\x2dxscreensaver-autostart@autostart.service.d/override.conf' \
+	"$dest/"'app-lxqt\x2dxscreensaver-autostart@autostart.service.d/override.conf'
+
+exit 0
